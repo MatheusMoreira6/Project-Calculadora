a1f71bb3b1b2dc45f5a896d44322385fd955991b
MatheusMoreira6 <matheus-moreira.ivi75@hotmail.com>
2020-02-09 15:04:09 -0400

    Initial commit

---
+++ Calculadora.c
@@ -0,0 +1,37 @@
+#include <stdio.h> /*Biblioteca de tratamento de entrada/saída.*/
+#include <stdlib.h> /*Biblioteca de implementação de Funções para diversas operações*/
+#include <locale.h> /*Biblioteca que especifica constantes de acordo com a localização específica, como moeda, data, etc.*/
+void main ()
+{
+    char C; /*Variável de caracteres alfanumérico*/
+    float A, B, D; /*Variável de números reais com precisão simples*/
+    setlocale(LC_ALL, "Portuguese"); /*LC_ALL Faz referência à todos os aspectos da localização*/
+    printf("\nCalculadora V1.0");
+    printf("\n\nInsira o primeiro valor:");
+        scanf("%f", &A);
+    printf("\nInsira o segundo valor:");
+        scanf("%f", &B);
+    printf("\nSelecione o tipo de operação(+,-,*,/):");
+        scanf("%s", &C);
+    if (C=='+') /* "==" Igual a*/
+    {
+        D=A+B;
+        printf("\n\nResultado:%g\n\n\n", D);
+    }
+    else if (C=='-')
+    {
+        D=A-B;
+        printf("\n\nResultado:%g\n\n\n", D);
+    }
+    else if (C=='*')
+    {
+        D=A*B;
+        printf("\n\nResultado:%g\n\n\n", D);
+    }
+    else if (C=='/')
+    {
+        D=A/B;
+        printf("\n\nResultado:%g\n\n\n", D);
+    }
+    system ("pause");
+}
