917e1e390dc6d30c487e8030f59b5ecc6a46c41d
MatheusMoreira6 <matheus-moreira.ivi75@hotmail.com>
2020-02-19 14:58:18 -0400

    Alteração no Código

--- Calculadora.c
+++ Calculadora.c
@@ -1,10 +1,10 @@
 #include <stdio.h> //Biblioteca de tratamento de entrada/saída.
 #include <stdlib.h> //Biblioteca de implementação de Funções para diversas operações.
 #include <locale.h> //Biblioteca que especifica constantes de acordo com a localização específica, como moeda, data, etc.
-void main ()
+int main ()
 {
     char C; //Variável de caracteres alfanumérico.
-    float A, B, D; //Variável de números reais com precisão simples.
+    float A, B, D; //Variável de números reais com precisão dupla.
     setlocale(LC_ALL, "Portuguese"); //LC_ALL Faz referência à todos os aspectos da localização.
     printf("\nCalculadora V1.0");
     printf("\n\nInsira o primeiro valor:");
@@ -17,21 +17,34 @@ void main ()
     {
         D=A+B;
         printf("\n\nResultado:%g\n\n\n", D);
+        system ("pause");
+        return (0);
     }
     else if (C=='-')
     {
         D=A-B;
         printf("\n\nResultado:%g\n\n\n", D);
+        system ("pause");
+        return (0);
     }
     else if (C=='*')
     {
         D=A*B;
         printf("\n\nResultado:%g\n\n\n", D);
+        system ("pause");
+        return (0);
     }
     else if (C=='/')
     {
         D=A/B;
         printf("\n\nResultado:%g\n\n\n", D);
+        system ("pause");
+        return (0);
     }
-    system ("pause");
+    else if (C!='+'||'-'||'*'||'/')
+    {
+        printf("\n\nOperação Inválida!\n\n\n");
+        system ("pause");
+        return (0);
+    }
 }
