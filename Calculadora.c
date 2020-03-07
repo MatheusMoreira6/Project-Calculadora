a22d975f00b5535d5746a59d09fbf951f347a84e
MatheusMoreira6 <matheus-moreira.ivi75@hotmail.com>
2020-03-07 15:28:29 -0400

    Melhoramento do Código

--- Calculadora.c
+++ Calculadora.c
@@ -1,50 +1,42 @@
 #include <stdio.h> //Biblioteca de tratamento de entrada/saída.
 #include <stdlib.h> //Biblioteca de implementação de Funções para diversas operações.
 #include <locale.h> //Biblioteca que especifica constantes de acordo com a localização específica, como moeda, data, etc.
+#include <conio.h>
 int main ()
 {
     char C; //Variável de caracteres alfanumérico.
     float A, B, D; //Variável de números reais com precisão dupla.
-    setlocale(LC_ALL, "Portuguese"); //LC_ALL Faz referência à todos os aspectos da localização.
-    printf("\nCalculadora V1.0");
+    setlocale(LC_ALL, "Portuguese"); //LC_ALL Faz referência à todos os aspectos de localização.
+    printf("Calculadora V3.0");
     printf("\n\nInsira o primeiro valor:");
         scanf("%f", &A);
     printf("\nSelecione o tipo de operação(+,-,*,/):");
-        scanf("%s", &C);
-    printf("\nInsira o segundo valor:");
-        scanf("%f", &B);
-    if (C=='+') /* "==" Igual a*/
+        C=getche();
+    if (C=='+' || C=='-' || C=='*' || C=='/')
     {
-        D=A+B;
+        printf("\n\nInsira o segundo valor:");
+            scanf("%f", &B);
+        if (C=='+')
+        {
+            D=A+B;
+        }
+        else if (C=='-')
+        {
+            D=A-B;
+        }
+        else if (C=='*')
+        {
+            D=A*B;
+        }
+        else if (C=='/')
+        {
+            D=A/B;
+        }
         printf("\n\nResultado:%g\n\n\n", D);
-        system ("pause");
-        return (0);
     }
-    else if (C=='-')
+    else
     {
-        D=A-B;
-        printf("\n\nResultado:%g\n\n\n", D);
-        system ("pause");
-        return (0);
+        printf("\n\nOperador Inválido!\n\n\n");
     }
-    else if (C=='*')
-    {
-        D=A*B;
-        printf("\n\nResultado:%g\n\n\n", D);
-        system ("pause");
-        return (0);
-    }
-    else if (C=='/')
-    {
-        D=A/B;
-        printf("\n\nResultado:%g\n\n\n", D);
-        system ("pause");
-        return (0);
-    }
-    else if (C!='+'||'-'||'*'||'/')
-    {
-        printf("\n\nOperação Inválida!\n\n\n");
-        system ("pause");
-        return (0);
-    }
+    system ("pause");
 }
