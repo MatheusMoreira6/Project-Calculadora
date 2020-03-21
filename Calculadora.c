f2f6f521d490600da629fd8fbb143ee6df1a9c45
MatheusMoreira6 <matheus-moreira.ivi75@hotmail.com>
2020-03-20 22:26:21 -0400

    Atualização (4.0)
    Substituido o sistema de "else if" para "switch".
    
    Adicionado estrutura de repetição para realização de novos calculos.

--- Calculadora.c
+++ Calculadora.c
@@ -2,45 +2,53 @@
 #include <stdlib.h> //Biblioteca de implementação de Funções para diversas operações.
 #include <locale.h> //Biblioteca que especifica constantes de acordo com a localização específica, como moeda, data, etc.
 #include <conio.h> //Biblioteca para utilização de funções como: getche
-#include <ctype.h>
 
-int main ()
+float main ()
 {
-    char C; //Variável de caracteres alfanumérico.
+    char C, V; //Variável de caracteres alfanumérico.
     float A, B, D; //Variável de números reais.
     setlocale(LC_ALL, "Portuguese"); //LC_ALL Faz referência à todos os aspectos de localização.
-    system("color A"); //Coloração (verde-claro).
-    printf("Calculadora V3.0");
-    printf("\n\nInsira o primeiro valor:");
-        scanf("%f", &A);
-    printf("\nSelecione o tipo de operação(+,-,*,/):");
-        C=getche();
-    if (C=='+' || C=='-' || C=='*' || C=='/')
-    {
-        printf("\n\nInsira o segundo valor:");
-            scanf("%f", &B);
-        if (C=='+')
+    do{
+        system("color 70"); //Coloração (verde-claro).
+        printf("Calculadora V4.0\n_________________________________________");
+        printf("\n\nInsira o primeiro valor:");
+            scanf("%f", &A);
+        printf("\nSelecione o tipo de operação(+,-,*,/):");
+            C=getche();
+        if (C=='+' || C=='-' || C=='*' || C=='/')
         {
-            D=A+B;
+            printf("\n\nInsira o segundo valor:");
+                scanf("%f", &B);
+            switch(C)
+            {
+                case '+':
+                    D=A+B;
+                    break;
+                case '-':
+                    D=A-B;
+                    break;
+                case '*':
+                    D=A*B;
+                    break;
+                case '/':
+                    D=A/B;
+                    break;
+            }
+            printf("_________________________________________\n\nResultado:%g\n_________________________________________\n", D);
+            printf("\nDeseja realizar um novo cálculo(S/N)?");
+                V=getche();
+            if (V=='S' || V=='s')
+            {
+                system("cls");
+            }
         }
-        else if (C=='-')
+        else
         {
-            D=A-B;
+            system("color 0C");
+            printf("\n\nErro: Operador Inválido!\n\n");
+            system("pause");
+            V='S';
+            system("cls");
         }
-        else if (C=='*')
-        {
-            D=A*B;
-        }
-        else if (C=='/')
-        {
-            D=A/B;
-        }
-        printf("\n\nResultado:%g\n\n\n", D);
-    }
-    else
-    {
-        system("color C");
-        printf("\n\nErro: Operador Inválido!\n\n\n");
-    }
-    system ("pause");
+    }while (V=='S' || V=='s');
 }
